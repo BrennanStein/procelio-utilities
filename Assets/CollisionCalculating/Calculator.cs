a4a2e9acbc249f4f19f25e0666bc288e44ca6455
Brennan Stein <brenn22@cs.washington.edu>
2018-06-26 20:48:01 -0700

    started my stuff

---
+++ Assets/CollisionCalculating/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour {
+    public GameObject prefab;
+    public List<GameObject> objects = new List<GameObject>();
+
+    public void ListEm()
+    {
+        foreach (Transform t in prefab.transform)
+        {
+            GameObject go = t.gameObject;
+            go.transform.parent = null;
+            go.transform.rotation = Quaternion.Euler(0, 0, 0);
+            go.transform.position = Vector3.zero;
+            go.AddComponent<MeshCollider>();
+            go.GetComponent<MeshCollider>().sharedMesh = go.GetComponent<MeshFilter>().mesh;
+            objects.Add(go);
+        }
+    } 
+}
