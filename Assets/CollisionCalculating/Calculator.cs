f22b5240a83b39e4bd3e21fc0cfc56a88f5e370c
Brennan Stein <brenn22@cs.washington.edu>
2018-06-26 21:37:56 -0700

    Maybe calculate collisions. Issues with touching, tho

--- Assets/CollisionCalculating/Calculator.cs
+++ Assets/CollisionCalculating/Calculator.cs
@@ -1,22 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class Calculator : MonoBehaviour {
+public class Calculator : MonoBehaviour
+{
     public GameObject prefab;
     public List<GameObject> objects = new List<GameObject>();
 
     public void ListEm()
     {
-        foreach (Transform t in prefab.transform)
+        var list = prefab.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < 10;/* list.Length;*/ ++i)
         {
-            GameObject go = t.gameObject;
+            GameObject go = list[i].gameObject;
             go.transform.parent = null;
             go.transform.rotation = Quaternion.Euler(0, 0, 0);
             go.transform.position = Vector3.zero;
-            go.AddComponent<MeshCollider>();
-            go.GetComponent<MeshCollider>().sharedMesh = go.GetComponent<MeshFilter>().mesh;
+            go.transform.localScale = new Vector3(1.01f,1.01f,1.01f);
             objects.Add(go);
         }
-    } 
+    }
+    public void OutputEnum()
+    {
+        string z = Application.persistentDataPath;
+        using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(z + "/ENUM.txt"))
+        {
+            file.WriteLine("enum CollisionPrimitives {");
+            foreach (var thing in objects)
+            {
+                file.WriteLine(thing.name + ",");
+            }
+            file.WriteLine("}");
+        }
+        Debug.Log("PRINTED");
+    }
+
+    public void Start()
+    {
+        ListEm();
+        OutputEnum();
+        StartCoroutine(CheckCollisions());
+    }
+
+    public IEnumerator CheckCollisions()
+    {
+        using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(Application.persistentDataPath + "/collisions.txt"))
+        {
+            file.WriteLine("bool[][] data = new bool[][] {");
+            Debug.Log(objects.Count);
+            for (int i = 0; i < objects.Count; ++i)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("/*" + objects[i].name + "*/ { ");
+                for (int j = 0; j < objects.Count; ++j)
+                {
+                    if (i == j)
+                    {
+                        sb.Append("true, ");
+                        continue;
+                    }
+                    objects[i].SetActive(true);
+                    objects[i].AddComponent<CollideTrack>();
+
+                    objects[i].AddComponent<MeshCollider>().convex = true;
+                    objects[i].AddComponent<Rigidbody>().isKinematic = true;
+                    objects[i].GetComponent<MeshCollider>().isTrigger = true;
+                    objects[i].GetComponent<MeshCollider>().sharedMesh = objects[i].GetComponent<MeshFilter>().mesh;
+                    objects[j].SetActive(true);
+                    objects[j].transform.position = new Vector3(10, 10, 10);
+                    objects[j].AddComponent<CollideTrack>();
+
+                    objects[j].AddComponent<MeshCollider>().convex = true;
+                    objects[j].AddComponent<Rigidbody>().isKinematic = true;
+                    objects[j].GetComponent<MeshCollider>().isTrigger = true;
+                    objects[j].GetComponent<MeshCollider>().sharedMesh = objects[j].GetComponent<MeshFilter>().mesh;
+                    objects[j].transform.position = Vector3.zero;
+                    yield return new WaitForSeconds(.05f);
+                    if (objects[i].GetComponent<CollideTrack>().col)
+                    {
+                        sb.Append("true, ");
+                    }
+                    else
+                        sb.Append("false,");
+                    objects[i].SetActive(false);
+                    objects[j].SetActive(false);
+                    DestroyImmediate(objects[i].GetComponent<CollideTrack>());
+                    DestroyImmediate(objects[i].GetComponent<MeshCollider>());
+                    DestroyImmediate(objects[j].GetComponent<CollideTrack>());
+                    DestroyImmediate(objects[j].GetComponent<MeshCollider>());
+                    DestroyImmediate(objects[i].GetComponent<Rigidbody>());
+                    DestroyImmediate(objects[j].GetComponent<Rigidbody>());
+                }
+                sb.Append("},");
+                file.WriteLine(sb.ToString());
+            }
+            file.WriteLine("};");
+        }
+        Debug.Log("CHECKED:)");
+    }
 }
---
+++ Assets/CollisionCalculating/CollideTrack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideTrack : MonoBehaviour {
+
+    public bool col = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRU");
+        col = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
