392af73461503bec2b01049ef35749c96855fb68
Brennan Stein <brenn22@cs.washington.edu>
2018-06-30 10:01:46 -0700

    S to save; made collider for big thruster

--- Assets/ColliderBuilder/ColliderBuilder.cs
+++ Assets/ColliderBuilder/ColliderBuilder.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ColliderBuilder : MonoBehaviour
 {
-  
+
 
     ColPriv partType = (ColPriv)0;
     Vector3Int position = Vector3Int.zero;
     List<KeyValuePair<Vector3Int, ColPriv>> shapes = new List<KeyValuePair<Vector3Int, ColPriv>>();
-
+    Dictionary<Vector3Int, GameObject> manifest = new Dictionary<Vector3Int, GameObject>();
     GameObject ghost;
     public Material whitetrans;
     public Material bluetrans;
@@ -30,12 +30,12 @@ public class ColliderBuilder : MonoBehaviour
     {
         ghost.transform.position = new Vector3(position.x, position.y, position.z);
         ghost.transform.rotation = Quaternion.identity;
-        if (Input.GetKeyDown(KeyCode.RightBracket))
+        if (Input.GetKeyDown(KeyCode.RightBracket) || Input.GetKeyDown(KeyCode.V))
         {
             partType = (ColPriv)(((int)partType + 1) % (int)ColPriv.COUNT);
             ResetGhost();
         }
-        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        if (Input.GetKeyDown(KeyCode.LeftBracket) || Input.GetKeyDown(KeyCode.C))
         {
             partType = (ColPriv)(((int)partType - 1 + (int)ColPriv.COUNT) % (int)ColPriv.COUNT);
             ResetGhost();
@@ -53,25 +53,30 @@ public class ColliderBuilder : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Comma))
             position.y--;
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { partType = ColPriv.CUBE; }          // 1: CUBE
-        if (Input.GetKeyDown(KeyCode.Alpha2)) { partType = ColPriv.PX_NY_PRISM; }   // 2: PRISM
-        if (Input.GetKeyDown(KeyCode.Alpha3)) { partType = ColPriv.PX_PZ_NY_TETRA; }// 3: TETRA
-        if (Input.GetKeyDown(KeyCode.Alpha4)) { partType = ColPriv.PX_PY_PZ_INNER; }// 4: INNER
-        if (Input.GetKeyDown(KeyCode.Alpha5)) { partType = ColPriv.PX_SLAB; }       // 5: HALFSLAB
-        if (Input.GetKeyDown(KeyCode.Alpha6)) { partType = ColPriv.PX_NY_COVER; }   // 6: PRISM COVER
-        if (Input.GetKeyDown(KeyCode.Alpha7)) { partType = ColPriv.PX_PZ_NY_COVER; }// 7: TETRA COVER
-        if (Input.GetKeyDown(KeyCode.Alpha8)) { partType = ColPriv.PX_PZ_NY_INCOV; }// 8: INNER COVER
-        if (Input.GetKeyDown(KeyCode.Alpha9)) { partType = ColPriv.X_PILLAR; }      // 9: MISC RECTANGLES
-        if (Input.GetKeyDown(KeyCode.Alpha0)) { partType = ColPriv.X_RIDGE_NY; }    // 0: CENTERED RIDGE
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { partType = ColPriv.CUBE; ResetGhost(); }          // 1: CUBE
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { partType = ColPriv.PX_NY_PRISM; ResetGhost(); }   // 2: PRISM
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { partType = ColPriv.PX_PZ_NY_TETRA; ResetGhost(); }// 3: TETRA
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { partType = ColPriv.PX_PY_PZ_INNER; ResetGhost(); }// 4: INNER
+        if (Input.GetKeyDown(KeyCode.Alpha5)) { partType = ColPriv.PX_SLAB; ResetGhost(); }       // 5: HALFSLAB
+        if (Input.GetKeyDown(KeyCode.Alpha6)) { partType = ColPriv.PX_NY_COVER; ResetGhost(); }   // 6: PRISM COVER
+        if (Input.GetKeyDown(KeyCode.Alpha7)) { partType = ColPriv.PX_PZ_NY_COVER; ResetGhost(); }// 7: TETRA COVER
+        if (Input.GetKeyDown(KeyCode.Alpha8)) { partType = ColPriv.PX_PZ_NY_INCOV; ResetGhost(); }// 8: INNER COVER
+        if (Input.GetKeyDown(KeyCode.Alpha9)) { partType = ColPriv.X_PILLAR; ResetGhost(); }      // 9: MISC RECTANGLES
+        if (Input.GetKeyDown(KeyCode.Alpha0)) { partType = ColPriv.X_RIDGE_NY; ResetGhost(); }    // 0: CENTERED RIDGE
 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Output();
+        }
 
-
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.X))
         {
             for (int i = 0; i < shapes.Count; ++i)
                 if (shapes[i].Key.Equals(position))
                 {
                     shapes.RemoveAt(i);
+                    Destroy(manifest[position]);
+                    manifest.Remove(position);
                     break;
                 }
         }
@@ -83,6 +88,26 @@ public class ColliderBuilder : MonoBehaviour
             shapes.Add(new KeyValuePair<Vector3Int, ColPriv>(position, partType));
             GameObject g = Instantiate(ghost);
             g.GetComponent<MeshRenderer>().material = whitetrans;
+            manifest.Add(position, g);
+        }
+    }
+
+    void Output()
+    {
+        using (System.IO.StreamWriter file =
+          new System.IO.StreamWriter(Application.persistentDataPath + "/colliderdata.txt"))
+        {
+            file.Write("KeyValuePair<ColPriv, int>[] array = {");
+            for (int i = 0; i < shapes.Count; ++i)
+            {
+                if (i != 0) file.Write(", "); // Suck it, traditional fencepost structure
+                file.Write("{ColPriv." + shapes[i].Value+", "+ BotData.BlockData.AssemblePos(shapes[i].Key.x, shapes[i].Key.y, shapes[i].Key.z)+"}");
+
+            }
+            file.Write("};");
+            List<ColPriv> lcp = new List<ColPriv>();
+            ColPriv[] dta = { ColPriv.CUBE, ColPriv.NX_NUB };
+            lcp.AddRange(dta);
         }
     }
 }
---
+++ Assets/ColliderBuilder/DataStructs.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace BotData
+{
+
+    public struct SBVector3
+    {
+        public sbyte x;
+        public sbyte y;
+        public sbyte z;
+        public SBVector3(int x, int y, int z)
+        {
+            this.x = (sbyte)x;
+            this.y = (sbyte)y;
+            this.z = (sbyte)z;
+        }
+        public override int GetHashCode()
+        {
+            int temp = ((int)x << 16) | ((int)y << 8) | ((int)z);
+            return temp.GetHashCode();
+        }
+    }
+
+    public delegate void BlockUpdate(BlockData data);
+    public sealed class MeshData
+    {
+        public int offset = 0;
+        public Vector3[] vertices;
+        public Vector3[] normals;
+        public Vector4[] tangents;
+        public Color32[] colors;
+        public Vector2[] uv1;
+        public Vector2[] uv2;
+        public int triIndex = 0;
+        public int[] triangles;
+        public MeshData(int numVertices = 65536, int numTrianglePoints = 62208)
+        {
+            vertices = new Vector3[numVertices];
+            normals = new Vector3[numVertices];
+            tangents = new Vector4[numVertices];
+            colors = new Color32[numVertices];
+            uv1 = new Vector2[numVertices];
+            uv2 = new Vector2[numVertices];
+            triangles = new int[numTrianglePoints];
+        }
+    }
+    public sealed class BlockData
+    {
+        public const int ZPOSMASK = 0xFF;
+        public const int YPOSMASK = 0xFF00;
+        public const int XPOSMASK = 0xFF0000;
+        //   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetX(int pos)
+        {
+            return (sbyte)((pos & XPOSMASK) >> 16);
+        }
+        //   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetY(int pos)
+        {
+            return (sbyte)((pos & YPOSMASK) >> 8);
+        }
+        // [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetZ(int pos)
+        {
+            return (sbyte)(pos & ZPOSMASK);
+        }
+        //  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int AssemblePos(int x, int y, int z)
+        {
+
+            int toReturn = ((byte)x) << 16 | ((byte)y) << 8 | ((byte)z << 0);
+            return toReturn;
+        }
+        /// [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int AssemblePos(SBVector3 vec)
+        {
+            return AssemblePos(vec.x, vec.y, vec.z);
+        }
+        //   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte AssembleRotation(Quaternion q)
+        {
+            return (byte)(((int)((q.eulerAngles.x + .5f) / 90) << 4) | ((int)((q.eulerAngles.y + .5f) / 90) << 2) | ((int)(q.eulerAngles.z + 0.5f) / 90));
+        }
+        // Represents the location of this block within the bot as signed XYZ.
+
+    }
+
+}
