6fb724735885d0e1751974cd1bb59b8ef1e0add8
Brennan Stein <brenn22@cs.washington.edu>
2018-12-27 14:56:42 -0800

    colliderbuilder

---
+++ Assets/ColliderBuilder/CamMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public float MovementSpeed = 15;
+    public float TurnMultiplier = 120;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    float xRot = 180;
+    float yRot = 0;
+    float zRot = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        bool forward = Input.GetKey(KeyCode.W); //Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Forward));
+        bool backward = Input.GetKey(KeyCode.S);// Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Backward));
+        bool left = Input.GetKey(KeyCode.A);// Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Left));
+        bool right = Input.GetKey(KeyCode.D); //Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Right));
+        bool up = Input.GetKey(KeyCode.Space); //Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Up));
+        bool down = Input.GetKey(KeyCode.LeftShift);// Settings.ControlsSettings.GetKey(Settings.ControlsSettings.GetInstanceCode(Settings.ControlsSettings.CONTROLS.Down));
+        Vector3 transFor = transform.forward;
+        if (forward || backward)
+        {
+            Vector3 move = transFor * (forward ? 1 : -1) * MovementSpeed * Time.deltaTime;
+            if (move.y < 0 ? up : down)
+                move = Vector3.ProjectOnPlane(move, Vector3.up);
+            else if (up != down) move += (up ? Vector3.up : Vector3.down) * MovementSpeed / 2 * Time.deltaTime;
+            transform.position += move;
+        }
+        else
+        {
+            if (up != down)
+                transform.position += (up ? Vector3.up : Vector3.down) * MovementSpeed / 2 * Time.deltaTime;
+        }
+
+        if (left)
+        {
+            transform.position += -transform.right * MovementSpeed * Time.deltaTime;
+        }
+
+        if (right)
+        {
+            transform.position += transform.right * MovementSpeed * Time.deltaTime;
+        }
+
+        float xrot = Time.deltaTime * TurnMultiplier * Input.GetAxis("Mouse X");
+        float yrot = Time.deltaTime * -TurnMultiplier * Input.GetAxis("Mouse Y");
+
+        xRot += xrot;
+        if (xRot > 360) xRot -= 360;
+        if (xRot < 0) xRot += 360;
+        yRot += yrot;
+        if (yRot < -89.999) yRot = -89.999f;
+        if (yRot > 89.999) yRot = 89.999f;
+        transform.rotation = Quaternion.Euler(yRot, xRot, zRot);
+
+    }
+}
--- Assets/ColliderBuilder/ColliderBuilder.cs
+++ Assets/ColliderBuilder/ColliderBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,9 +15,15 @@ public class ColliderBuilder : MonoBehaviour
     public Material whitetrans;
     public Material bluetrans;
 
+    public GameObject cameraObj;
+    public float distance;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         ResetGhost();
+        cameraObj = Camera.main.gameObject;
+        distance = 10;
     }
     void ResetGhost()
     {
@@ -28,7 +35,10 @@ public class ColliderBuilder : MonoBehaviour
     }
     void Update()
     {
-        ghost.transform.position = new Vector3(position.x, position.y, position.z);
+        distance += Input.GetAxis("Mouse ScrollWheel");
+        Vector3 ppp = cameraObj.transform.position + cameraObj.transform.forward * distance;
+        position = new Vector3Int((int)ppp.x, (int)ppp.y, (int)ppp.z);
+        ghost.transform.position = position;
         ghost.transform.rotation = Quaternion.identity;
         if (Input.GetKeyDown(KeyCode.RightBracket) || Input.GetKeyDown(KeyCode.V))
         {
@@ -64,12 +74,19 @@ public class ColliderBuilder : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha9)) { partType = ColPriv.X_PILLAR; ResetGhost(); }      // 9: MISC RECTANGLES
         if (Input.GetKeyDown(KeyCode.Alpha0)) { partType = ColPriv.X_RIDGE_NY; ResetGhost(); }    // 0: CENTERED RIDGE
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
         {
+            Debug.LogError("SAVED MODEL");
+            Output();
+        }
+        if (Input.GetKey(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.LogError("Saving from gameobjects");
+            LoadShapesFromGameObjects();
             Output();
         }
 
-        if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Mouse1))
         {
             for (int i = 0; i < shapes.Count; ++i)
                 if (shapes[i].Key.Equals(position))
@@ -80,7 +97,7 @@ public class ColliderBuilder : MonoBehaviour
                     break;
                 }
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             foreach (var t in shapes)
                 if (t.Key == position)
@@ -92,6 +109,25 @@ public class ColliderBuilder : MonoBehaviour
         }
     }
 
+    private void LoadShapesFromGameObjects()
+    {
+        shapes.Clear();
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+            if (go.activeInHierarchy && (go != ghost))
+            {
+                if (go.name.IndexOf("(Clone)") < 0) continue;
+                string name = go.name.Substring(0, go.name.IndexOf("(Clone)"));
+                ColPriv type = (ColPriv)Enum.Parse(typeof(ColPriv), name, true);
+                var posKey = new Vector3Int((int)(go.transform.position.x + .5f),
+                    (int)(0.5f + go.transform.position.y), (int)(.5f + go.transform.position.z));
+                foreach (var t in shapes)
+                    continue;
+                shapes.Add(new KeyValuePair<Vector3Int, ColPriv>(posKey, type));
+            }
+        Debug.LogError("SaveSuccess");
+    }
+
     void Output()
     {
         using (System.IO.StreamWriter file =
@@ -101,7 +137,7 @@ public class ColliderBuilder : MonoBehaviour
             for (int i = 0; i < shapes.Count; ++i)
             {
                 if (i != 0) file.Write(", "); // Suck it, traditional fencepost structure
-                file.Write("{ColPriv." + shapes[i].Value+", "+ BotData.BlockData.AssemblePos(shapes[i].Key.x, shapes[i].Key.y, shapes[i].Key.z)+"}");
+                file.Write("new KeyValuePair<ColPriv, int>(ColPriv." + shapes[i].Value + ", " + BotData.BlockData.AssemblePos(shapes[i].Key.x, shapes[i].Key.y, shapes[i].Key.z) + ")");
 
             }
             file.Write("};");
